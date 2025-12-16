bddf47c369827a1fa85860a95356d1bb9118912d
A R Danish <helloardanish@gmail.com>
2025-12-16 22:30:19 +0530

    Testing values updated

--- helper/JsonOperation.py
+++ helper/JsonOperation.py
@@ -1,22 +1,22 @@
 import json
+import copy
 
 class JsonOperation:
     def __init__(self, file_path):
         print("JSON Basic Operations")
         self.file_path = file_path
 
-    def extract_all_values_from_json_file(self):
-        with open(self.file_path, "r") as file:
-            data = json.load(file)
+    def load_json(self):
+        with open(self.file_path, "r", encoding="utf-8") as file:
+            return json.load(file)
 
-        values = []
+    def extract_unique_values(self):
+        data = self.load_json()
+        values = set()
         self._extract_values(data, values)
-        return values
+        return list(values)
 
     def _extract_values(self, data, values):
-        """
-        Recursively traverse JSON and collect only values
-        """
         if isinstance(data, dict):
             for value in data.values():
                 self._extract_values(value, values)
@@ -24,10 +24,34 @@ class JsonOperation:
             for item in data:
                 self._extract_values(item, values)
         else:
-            values.append(data)
+            values.add(data)
 
+    def replace_values_and_save(self, translation_map, output_file):
+        """
+        Replaces only exact value matches (quoted values),
+        keys are never modified.
+        """
+        original_data = self.load_json()
+        new_data = copy.deepcopy(original_data)
 
-# json_ops = JsonOperation("./en.json")
-# result = json_ops.extract_all_values_from_json_file()
-# print(result)
+        self._replace_values(new_data, translation_map)
 
+        with open(output_file, "w", encoding="utf-8") as file:
+            json.dump(new_data, file, ensure_ascii=False, indent=2)
+
+    def _replace_values(self, data, translation_map):
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if isinstance(value, (dict, list)):
+                    self._replace_values(value, translation_map)
+                else:
+                    if value in translation_map:
+                        data[key] = translation_map[value]
+
+        elif isinstance(data, list):
+            for i, item in enumerate(data):
+                if isinstance(item, (dict, list)):
+                    self._replace_values(item, translation_map)
+                else:
+                    if item in translation_map:
+                        data[i] = translation_map[item]
--- webpage/GoogleTranslate.py
+++ webpage/GoogleTranslate.py
@@ -24,13 +24,21 @@ class GoogleTranslate:
 
     def file_process(self):
         logger.info("Process file values for converting")
-        return self.json_ops.extract_all_values_from_json_file()
+        return self.json_ops.extract_unique_values()
         
 
     def start(self):
-        all_values = self.file_process()
-        logger.info(result)
+        all_unique_values = self.file_process()
+        logger.info(all_unique_values)
         self.driver.get(self.URL)
+        translations = {}
+        for value in all_unique_values:
+            translations[value] = "4.1.3 ¿Cómo estás?"
+
+        self.json_ops.replace_values_and_save(
+            translation_map=translations,
+            output_file="output_es.json"
+        )
         # self.select_languages()
 
 
